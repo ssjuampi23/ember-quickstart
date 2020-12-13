8570f22c90b0bd32a13a73cd2c1edfa34e42f349
ssjuampi23 <sspablo23@hotmail.com>
2020-12-13 08:26:31 -0600

    Create Scientists component, route and template

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default class ScientistsRoute extends Route {
+  // The model() method supports any library that uses JS Promises like axios
+  model(){
+    // Here you can return whatever data you want to make available to the scientists.hbs template
+    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
+  }
 }
